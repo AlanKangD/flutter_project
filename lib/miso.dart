f8c2716ca1fafc7510171d3f0c4dc898aa8aaea1
alan <kno12350@gmail.com>
2024-03-11 16:28:19 +0900

    first Step 미소 첫번째 화면 텍스트 레이아웃 구성

---
+++ lib/miso.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+/// Miso 메인 색상
+Color misoPrimaryColor = Color.fromARGB(255, 38, 103, 240);
+
+class Miso extends StatefulWidget {
+  const Miso({Key? key}) : super(key: key);
+
+  @override
+  _MisoState createState() => _MisoState();
+}
+
+class _MisoState extends State<Miso> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex, // index 순서에 해당하는 child를 맨 위에 보여줌
+        children: [
+          MisoFirstPage(),
+          MisoSecondPage(),
+          MisoThirdPage(),
+          MisoFourthPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex, // 현재 보여주는 탭
+        onTap: (newIndex) {
+          print("selected newIndex : $newIndex");
+          // 다른 페이지로 이동
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        selectedItemColor: misoPrimaryColor, // 선택된 아이콘 색상
+        unselectedItemColor: Colors.grey, // 선택되지 않은 아이콘 색상
+        showSelectedLabels: false, // 선택된 항목 label 숨기기
+        showUnselectedLabels: false, // 선택되지 않은 항목 label 숨기기
+        type: BottomNavigationBarType.fixed, // 선택시 아이콘 움직이지 않기
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.redeem), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        ],
+      ),
+    );
+  }
+}
+
+/// 첫 번째 페이지
+class MisoFirstPage extends StatelessWidget {
+  const MisoFirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: misoPrimaryColor, // 백그라운드 설정 먼저 진행하는것이 좋아보임
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  Text(
+                    "대한민국 1등 홈서비스\n미소를 만나보세요!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 38,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// 두 번째 페이지
+class MisoSecondPage extends StatelessWidget {
+  const MisoSecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 두 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 세 번째 페이지
+class MisoThirdPage extends StatelessWidget {
+  const MisoThirdPage({Key? key}) : super(key: key);
+
+  /// 세 번째 화면 배경 이미지 URL
+  final String backgroundImgUrl =
+      "https://i.ibb.co/rxzkRTD/146201680-e1b73b36-aa1e-4c2e-8a3a-974c2e06fa9d.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 세 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 네 번째 페이지
+class MisoFourthPage extends StatelessWidget {
+  const MisoFourthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 네 번째 페이지"),
+      ),
+    );
+  }
+}
