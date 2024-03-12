23aaa076362c5222567929df45985af233d1676d
alan <kno12350@gmail.com>
2024-03-12 09:36:00 +0900

    miso project 두번째 영역 구현 완료

--- lib/miso.dart
+++ lib/miso.dart
@@ -171,10 +171,47 @@ class MisoSecondPage extends StatelessWidget {
                         fontSize: 32,
                         fontWeight: FontWeight.w900,
                       ),
+                    ),
+
+                    SizedBox(
+                      height: 64,
+                    ),
+                    // 아이콘
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.error,
+                          color: misoPrimaryColor,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+
+                        //예약된 서비스가 아직 없어요. 지금 예약해보세요
+                        // Tip : 폰의 폭과 관계없이 텍스트를 언제나 한 줄로 보여주기
+                        Expanded(
+                          // row의 남은 영역을 모두 차지하도록
+                          child: FittedBox(
+                            // 해당 영역의 적절한 사이즈로 맞추기
+                            fit: BoxFit.scaleDown, // child 최소 크기 존중
+                            child: Text(
+                              "예약된 서비스가 아직 없어요. 지금 예약해보세요!",
+                              style: TextStyle(
+                                fontSize: 100, // 아무리 커도 한줄로 보입니다.
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+
+                    // 밑줄 삽입
+                    Divider(
+                      color: Colors.grey,
                     )
                   ],
                 ),
-              )
+              ),
             ],
           )),
     ));
